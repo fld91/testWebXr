6d2920bff1a60c32ca09036ddff9a509deaf70c7
shrihari pardhikar <shrihari@devbeeit.com>
2022-10-31 13:42:49 +0530

    setupvr

--- app.js
+++ app.js
@@ -20,6 +20,42 @@ class App{
         const light = new THREE.DirectionalLight( 0xffffff );
         light.position.set( 1, 1, 1 ).normalize();
 		this.scene.add( light );
+
+      
+
+				// const texture = new THREE.VideoTexture( video );
+
+				// const geometry = new THREE.PlaneGeometry( 16, 9 );
+				// geometry.scale( 0.5, 0.5, 0.5 );
+				// const material = new THREE.MeshBasicMaterial( { map: texture } );
+                // const mesh = new THREE.Mesh( geometry, material );
+                // mesh.lookAt( this.camera.position );
+				// this.scene.add( mesh );
+
+				
+
+                // if ( navigator.mediaDevices && navigator.mediaDevices.getUserMedia ) {
+
+				// 	const constraints = { video: { width: 1280, height: 720, facingMode: 'user' } };
+
+				// 	navigator.mediaDevices.getUserMedia( constraints ).then( function ( stream ) {
+
+				// 		// apply the stream to the video element used in the texture
+
+				// 		video.srcObject = stream;
+				// 		video.play();
+
+				// 	} ).catch( function ( error ) {
+
+				// 		console.error( 'Unable to access the camera/webcam.', error );
+
+				// 	} );
+
+				// } else {
+
+				// 	console.error( 'MediaDevices interface not available.' );
+
+				// }
 			
 		this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true } );
 		this.renderer.setPixelRatio( window.devicePixelRatio );
@@ -54,12 +90,46 @@ class App{
         function onSelect() {
             const material = new THREE.MeshPhongMaterial( { color: 0xffffff * Math.random() } );
             const mesh = new THREE.Mesh( self.geometry, material );
-            mesh.position.set( 0, 0, - 0.3 ).applyMatrix4( controller.matrixWorld );
+            mesh.position.set( 0, 0, -0.09 ).applyMatrix4( controller.matrixWorld );
             mesh.quaternion.setFromRotationMatrix( controller.matrixWorld );
             self.scene.add( mesh );
             self.meshes.push( mesh );
 
         }
+       const texture = new THREE.VideoTexture( video );
+
+				const geometry = new THREE.PlaneGeometry( 5, 3 );
+				geometry.scale( 0.3, 0.3, 0.3 );
+				const material = new THREE.MeshBasicMaterial( { map: texture } );
+                const mesh = new THREE.Mesh( geometry, material );
+                mesh.lookAt( this.camera.position );
+				this.scene.add( mesh );
+
+				
+
+                if ( navigator.mediaDevices && navigator.mediaDevices.getUserMedia ) {
+
+					const constraints = { video: { width: 900, height: 450, facingMode: 'user' } };
+					// const constraints = { video: { width: 1280, height: 720, facingMode: 'user' } };
+
+					navigator.mediaDevices.getUserMedia( constraints ).then( function ( stream ) {
+
+						// apply the stream to the video element used in the texture
+
+						video.srcObject = stream;
+						video.play();
+
+					} ).catch( function ( error ) {
+
+						console.error( 'Unable to access the camera/webcam.', error );
+
+					} );
+
+				} else {
+
+					console.error( 'MediaDevices interface not available.' );
+
+				}
 
         const btn = new ARButton( this.renderer );
         
@@ -78,7 +148,7 @@ class App{
     
 	render( ) {   
         this.stats.update();
-        this.meshes.forEach( (mesh) => { mesh.rotateY( 0.01 ); });
+        // this.meshes.forEach( (mesh) => { mesh.rotateY( 0.01 ); });
         this.renderer.render( this.scene, this.camera );
     }
 }
